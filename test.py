dce0b75cee61c58cdcc899f7656dd42d30a2b9b1
Tian_Z <113085728+Singularity-tian@users.noreply.github.com>
2024-07-16 10:31:47 +0000

    new_front_end

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ from camel.types import ModelPlatformType, ModelType
 from camel.models import ModelFactory
 from camel.loaders import UnstructuredIO
 from camel.configs import ChatGPTConfig
-from unstructured.partition.auto import partition
 from camel.storages import Neo4jGraph
 
 # Set Neo4j instance
@@ -57,46 +56,31 @@ def generate_response(input_text):
     # Display the response
     st.info(kg_agent.run(graph_elements))
 
-def generate_response_file(file):
-    # Model setting
-    model = ModelFactory.create(
-        model_platform=ModelPlatformType.OPENAI,
-        model_type=ModelType.GPT_4O,
-        model_config_dict=ChatGPTConfig().__dict__,
-        api_key=openai_api_key
-    )
-    
-    # Agent setting
-    kg_agent = KnowledgeGraphAgent(model=model)
 
-    # Create an element from the given text
-    elements = partition(file=uploaded_file)
-
-    # Run the KnowledgeGraphAgent to generate graph elements
-    graph_elements = kg_agent.run(elements, parse_graph_elements=True)
-
-    # Add the elements to the Neo4j database
-    n4j.add_graph_elements(graph_elements=[graph_elements])
-
-    # Display the response
-    st.info(kg_agent.run(graph_elements))
 
 # Form for user input
 with st.form('my_form'):
-    uploaded_files = st.file_uploader("Choose a PDF file", type=['pdf'], accept_multiple_files=True)
-    print(type(uploaded_files))
-    print("@@@@@@@@@@@@")
-    flag = 0
-    for uploaded_file in uploaded_files:
-        flag = 1
-        generate_response_file(file=uploaded_file)
     
+    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        pdf_reader = PdfReader(uploaded_file)
+        text_pdf = ""
+        for page in pdf_reader.pages:
+            text_pdf += page.extract_text()
+        
+        # Display the filename and extracted text
+        st.write("filename:", uploaded_file.name)
+        st.write(text_pdf)
+    submitted1 = st.form_submit_button('Submit1')
 
     text = st.text_area('Enter text:', text_example)
-    submitted = st.form_submit_button('Submit')
+    submitted2 = st.form_submit_button('Submit2')
     
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     
-    if flag and submitted and openai_api_key.startswith('sk-'):
+    if submitted1 and openai_api_key.startswith('sk-'):
+        generate_response(text_pdf)
+
+    if submitted2 and openai_api_key.startswith('sk-'):
         generate_response(text)
