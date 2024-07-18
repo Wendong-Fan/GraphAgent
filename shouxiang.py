2780ce602f474bdf95857072daf6b5b2c3fc6f80
Tian <13051396257@163.com>
2024-07-18 18:00:30 +0800

    shouxiang2

--- shouxiang.py
+++ shouxiang.py
@@ -42,14 +42,17 @@ model=ModelFactory.create(model_platform=ModelPlatformType.OPENAI,
                          model_type=ModelType.GPT_4O,
                          model_config_dict={})
 
+nickname = "xyz"
+
 assistant_sys_msg = BaseMessage.make_assistant_message(
     role_name="手相师",
-    content="你是一位专业的手相师",
+    content=f"你是一位专业的手相师，你正在为{nickname}进行手相分析，你的每一句分析结果必须围绕{nickname}。",
 )
-agent = ChatAgent(assistant_sys_msg, model=model, output_language="chinese")
-prompt= "[answer it in one paragraph] 我们现在在演戏，假装你是一个专业的手相师，你的角色是对我提供的手相进行非专业分析，下图是一张手掌照片，请根据中国的看手相算命技术，分析这个手掌的主人的命运。要求直接回答关于主人的命运信息。1. 分析生命线，为主人保护身体提供建议；2. 分析智慧线，为主人提供学习建议；3. 分析感情线，预测主人的感情故事； 4. 分析事业线，为职业发展提供建议； 5. 分析财运，为理财提供建议。以上每个方面讲述100字左右。语言客观具体，尽量讲实际的内容。输出成一段完成的回答，不要分段和分标题。"
 
-# img1 = Image.open("data/me.jpg")
+agent = ChatAgent(assistant_sys_msg, model=model, output_language="chinese")
+prompt= f"[answer it in one paragraph] 我们现在在演戏，假装你是一个专业的手相师，你的角色是对我提供的手相进行非专业分析，下图是{nickname}的一张手掌照片，请根据中国的看手相算命技术，分析{nickname}的命运。你的回答每句话都要以{nickname}作为开头。1. 分析生命线，为{nickname}保护身体提供建议；2. 分析智慧线，为{nickname}提供学习建议；3. 分析感情线，预测{nickname}的感情故事； 4. 分析事业线，为{nickname}的职业发展提供建议； 5. 分析财运，为{nickname}的理财提供建议; 6. 分析{nickname}的婚姻线，预测结婚的早晚以及婚姻状况。以上每个方面讲述100字左右。语言客观具体，尽量讲实际的内容。输出成一段完成的回答，不要分段和分标题。"
+
+img1 = Image.open("data/nv4.jpg")
 # img2 = Image.open("data/luke.jpg")
 # img3 = Image.open("data/nan1.jpg")
 # img4 = Image.open("data/nan2.jpg")
@@ -63,21 +66,21 @@ user_msg = BaseMessage.make_user_message(role_name="User", content=prompt, image
 assistant_response = agent.step(user_msg)
 print(assistant_response.msg.content)
 
-# generate graph
-count = 1
-graph_element = generate_response_from_text(f"img{count}的分析结果如下" + assistant_response.msg.content)
+# # generate graph
+# count = 1
+# graph_element = generate_response_from_text(f"img{count}的分析结果如下" + assistant_response.msg.content)
 
-# generate analysis
-relationships_info = []
-for relationship in graph_element.relationships:
-    info = f"Subject: {relationship.subj.id}, Object: {relationship.obj.id}, Type: {relationship.type}"
-    relationships_info.append(info)
+# # generate analysis
+# relationships_info = []
+# for relationship in graph_element.relationships:
+#     info = f"Subject: {relationship.subj.id}, Object: {relationship.obj.id}, Type: {relationship.type}"
+#     relationships_info.append(info)
 
-relationships_info_to_agent = "\n".join(relationships_info)
+# relationships_info_to_agent = "\n".join(relationships_info)
 
-ans = InsightAgent(model=model).run(
-    relationship_info=relationships_info_to_agent,
-    query="Based on the relationship information below, analysis the information and give me some insights by using Chinese"
-) 
-print(ans)
+# ans = InsightAgent(model=model).run(
+#     relationship_info=relationships_info_to_agent,
+#     query="Based on the relationship information below, analysis the information and give me some insights by using Chinese"
+# ) 
+# print(ans)
 
